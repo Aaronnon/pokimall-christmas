fb410638e32badd8038370cdbd2dc1e26ee95695
Aaronnon <59756027+Aaronnon@users.noreply.github.com>
2020-12-15 19:55:29 -0500

    cloud server added

--- miniprogram/pages/main/index.js
+++ miniprogram/pages/main/index.js
@@ -1,4 +1,6 @@
 // miniprogram/pages/index.js
+const app = getApp()
+const db = wx.cloud.database()
 
 
 Page({
@@ -11,19 +13,50 @@ Page({
     clickRules: false,
     clickRewards: false,
     clickContent: false,
-    clickDone: false
+    clickContentDone:false,
+    clickDone: false,
+    progressPercent: 0,
+    status: false,
+
+
+    wish: '',
+    weChat: '',
+    phone: '',
+    tag:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {},
+  onLoad: function (options) {
+
+  },
 
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {}
+    }).then((res) => {
+      db.collection('users').where({
+        _openid: res.result.openid
+      }).get().then((res) => {
+        if (res.data.length) {
+          app.userInfo = Object.assign(app.userInfo, res.data[0])
+          this.setData({
+            status: true,
+            progressPercent: app.userInfo._percent,
+            weChat: app.userInfo._weChat,
+            phone: app.userInfo._phone,
+            wish: app.userInfo._wish,
+            tag: app.userInfo._tag
+          })
+        }
+      })
+    })
 
   },
 
@@ -48,24 +81,25 @@ Page({
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {},
+  onShareAppMessage: function () {
+    const newProgress = this.data.progressPercent + 0.1
+    const newProgressStr = newProgress.toString()
+    const index = newProgressStr.indexOf('.')
+    const result = Number(newProgressStr.slice(0, index + 2))
+
+    this.setData({
+      progressPercent: result
+    })
+    db.collection('users').doc(app.userInfo._id).update({
+      data: {
+        _percent: result
+      }
+    })
+  },
 
   onClickRules() {
     this.setData({
@@ -78,24 +112,72 @@ Page({
       clickRewards: true
     })
   },
+
+  
+
   onClickContent() {
-    this.setData({
-      clickContent: true
-    })
+    if(app.userInfo._wishRound){
+      this.setData({
+        clickContent: true
+      })
+    }else{
+      this.setData({
+        clickContentDone: true
+      })
+    }
+
   },
   clickValue(e) {
+    const newWish = e.detail.value
     if (e.detail.value !== '') {
-      console.log(e.detail.value);
+      this.setData({
+        wish: newWish
+      });
+      db.collection('users').doc(app.userInfo._id).update({
+        data: {
+          _wish: newWish
+        }
+      })
+    }
+
+  },
+  clickWechat(e) {
+    const newWechat = e.detail.value
+    if (e.detail.value !== '') {
+      this.setData({
+        weChat: newWechat
+      });
+      db.collection('users').doc(app.userInfo._id).update({
+        data: {
+          _weChat: newWechat
+        }
+      })
+    }
+
+  },
+  clickPhone(e) {
+    const newPhone = e.detail.value
+    if (e.detail.value !== '') {
+      this.setData({
+        phone: newPhone
+      });
+      db.collection('users').doc(app.userInfo._id).update({
+        data: {
+          _phone: newPhone
+        }
+      })
     }
 
   },
 
   confirmContert() {
+    
 
-    var str = "abcdefghijklmnopqrstuvwxyz0123456789";
+    var str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     // var arr = [...str]
     var tmp = [];
     var random;
+    var tmpJoin = ''
     for (var i = 0; i < 8; i++) {
       random = Math.floor(Math.random() * (str.length));
       if (tmp.indexOf(str[random]) === -1) {
@@ -104,13 +186,19 @@ Page({
         i--;
       }
     }
-    // tmp.toString();
-    console.log(tmp);
-    
+    tmpJoin = tmp.join('')
+    console.log(tmpJoin);
+
     this.setData({
       clickContent: false,
-      clickDone: true
-
+      clickDone: true,
+      tag:tmpJoin
+    })
+    db.collection('users').doc(app.userInfo._id).update({
+      data: {
+        _wishRound: 0,
+        _tag:tmpJoin
+      }
     })
 
   },
@@ -120,9 +208,38 @@ Page({
       clickRules: false,
       clickRewards: false,
       clickContent: false,
-      clickDone: false
-
+      clickDone: false,
+      clickContentDone:false
     })
+  },
+
+  getUserInfo(e) {
+    let userInfo = e.detail.userInfo
+    if (!this.data.status && userInfo) {
+      db.collection('users').add({
+        data: {
+          userPhoto: userInfo.avatarUrl,
+          nickName: userInfo.nickName,
+          _wishRound: 1,
+          _percent: 0,
+          _wish: '',
+          _weChat: '',
+          _phone: '',
+          _tag:'',
+          joinTime: new Date(),
+        }
+      }).then((res) => {
+        db.collection('users').doc(res._id).get().then((res) => {
+          app.userInfo = Object.assign(app.userInfo, res.data)
+          this.setData({
+            status: true,
+            clickContent: true
+          })
+
+        })
+
+      })
+    }
   }
 
 })
